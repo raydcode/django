d20b4e1fef1e5722f690f478033214679a7c2bf8
kathir2602 <kathir@crayond.co>
2022-01-30 13:17:46 +0530

    readme added

--- raydcode/polls/urls.py
+++ raydcode/polls/urls.py
@@ -1,13 +1,11 @@
-from django.urls import  path
-
+from django.urls import path
 
 from . import views
 
-
 app_name = 'polls'
 urlpatterns = [
-    path('', views.index,name="index"),
-    path('<int:question_id>/',views.detail,name="detail"),
-    path('<int:question_id>/result',views.result,name="result"),
+    path('', views.index, name='index'),
+    path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:question_id>/results/', views.results, name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote')
 ]
--- raydcode/polls/views.py
+++ raydcode/polls/views.py
@@ -1,45 +1,37 @@
-
 from django.template import loader
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 
+from .models import Question, Choice
 
-# Create your views here.
+# Get questions and display them
+def index(request):
+    latest_question_list = Question.objects.order_by('-publish_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
 
-from .models import Question,Choice
+# Show specific question and choices
+def detail(request, question_id):
+  try:
+    question = Question.objects.get(pk=question_id)
+  except Question.DoesNotExist:
+    raise Http404("Question does not exist")
+  return render(request, 'polls/detail.html', { 'question': question })
 
-
-
-# Get questions 
-
-def  index(request):
-    latest_questions_list = Question.objects.order_by('-publish_date')[:5]
-    context = {'latest_question_list':latest_questions_list}
-    return render(request,'polls/index.html',context)
-
-
-def detail(request,question_id):
-    try:
-       question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question doesn not exist")   
-    return render(request,'polls/detail.html',{'question':question})
-
-
-def result(request,question_id):
-    question = get_object_or_404(Question,pk=question_id)
-    return render(request,'polls/result.html',{'question':question})
-
+# Get question and display results
+def results(request, question_id):
+  question = get_object_or_404(Question, pk=question_id)
+  return render(request, 'polls/results.html', { 'question': question })
 
 # Vote for a question choice
 def vote(request, question_id):
-  
+    # print(request.POST['choice'])
     question = get_object_or_404(Question, pk=question_id)
     try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
-     
+        # Redisplay the question voting form.
         return render(request, 'polls/detail.html', {
             'question': question,
             'error_message': "You didn't select a choice.",
@@ -47,5 +39,7 @@ def vote(request, question_id):
     else:
         selected_choice.votes += 1
         selected_choice.save()
-        
-        return HttpResponseRedirect(reverse('polls:result', args=(question.id,)))
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
